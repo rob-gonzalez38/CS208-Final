347bbd96c040b7d6f072a626beeaa5bc229828e1
Rob Gonzalez <robertgonzalez@u.boisestate.edu>
2026-04-23 16:36:03 +0000

    Routes for menu, about, comments

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
+  res.render('index', { title: 'Home' });
 });
+
+/* Menu page */
+router.get('/menu', function(req, res, next) {
+  res.render('menu', { title: 'Menu' });
+});
+
+/* About page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+/* Comments page */
+router.get('/comments', function(req, res, next) {
+  res.render('comments', { title: 'Customer Comments' });
+});
+
 
 router.post('/create', function (req, res, next) {
     const { task } = req.body;
